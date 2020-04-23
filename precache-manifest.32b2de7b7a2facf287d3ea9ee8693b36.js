46d2c57eafa41bc1e2425929a280a9cb58d4531d
engel-m <magicnerdgamer@gmail.com>
2020-04-23 20:00:04 +0200

    Updates

---
+++ precache-manifest.32b2de7b7a2facf287d3ea9ee8693b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5e933c52cb5fed1cdb1050590a6187",
+    "url": "/road-accountant/index.html"
+  },
+  {
+    "revision": "bd5df6309cb0a651bbb2",
+    "url": "/road-accountant/static/css/main.cc108d5e.chunk.css"
+  },
+  {
+    "revision": "53cfc5e31b7530b3983c",
+    "url": "/road-accountant/static/js/2.b5118144.chunk.js"
+  },
+  {
+    "revision": "bad6e083d4d2a996fe60e513745a4089",
+    "url": "/road-accountant/static/js/2.b5118144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd5df6309cb0a651bbb2",
+    "url": "/road-accountant/static/js/main.d0289e54.chunk.js"
+  },
+  {
+    "revision": "0687f7d70514d9ea5e31",
+    "url": "/road-accountant/static/js/runtime-main.afee60eb.js"
+  }
+]);
